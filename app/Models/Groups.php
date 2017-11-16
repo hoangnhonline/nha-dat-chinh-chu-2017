39cff6ffd118c9e30c0c4a4acf168a05a35dceb7
dqtien <doquyettien@gmail.com>
2017-11-16 15:05:23 +0700

    Modal

---
+++ app/Models/Groups.php
@@ -0,0 +1,40 @@
+<?php namespace App\Models;
+
+use App\Models\Traits\BasicBehavior;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Groups extends Model
+{
+
+    use BasicBehavior;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'groups';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'type',
+        'status',
+        'created_user',
+        'updated_user'
+    ];
+
+}
